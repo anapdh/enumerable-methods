0359e70d83f8619851867fd6b649ed19d7d3a5cb
anapdh <ana_d_hubner@hotmail.com>
2020-11-18 19:39:25 -0300

    add count, map, inject and multiply mehods

--- enumerables.rb
+++ enumerables.rb
@@ -40,23 +40,60 @@ module Enumerable
     end
 
     def my_count(arg = nil)
-            @arr.length
             count = 0
+            if arg == nil && block_given? == false
+            return @arr.length    
+            elsif block_given?
+                my_each do |n|
+                    if yield(n)
+                    count += 1
+                    end
+                end
+                return count
+            else
             my_each do |n|
                 if arg == n
                 count += 1
                 end
             end
-            return count   
+            return count
+        end  
     end
 
-end
+    def my_map
+        newarr = []
+        my_each_with_index do | n, i |
+            newarr[i] = yield(n)
+        end
+        return newarr
+    end
+
+    def my_inject
+        acc = nil
+        my_each_with_index do | n, i |
+            if i == 0
+            acc = n
+            else
+            acc = yield(acc, n)
+        end
+        end
+        return acc
+    end
+    end
 
 class Arr
     include Enumerable
     def initialize(arr)
         @arr = arr
     end
+end
+
+def multiply_els(arg)
+    a = Arr.new(arg)
+    g = a.my_inject do | acc, n |
+        acc * n
+    end
+    return g
 end
 
 a = Arr.new([1, 2, 3])
@@ -89,3 +126,23 @@ puts "#{d}"
 puts "#{a.my_count}"
 
 puts "#{a.my_count(3)}"
+
+e = a.my_count do |n|
+    false
+end
+
+puts e
+
+f = a.my_map do | n |
+    n + n
+end
+
+puts "#{f}"
+
+g = a.my_inject do | acc, n |
+    acc * n
+end
+
+puts "#{g}"
+
+puts multiply_els([2, 4, 5])
